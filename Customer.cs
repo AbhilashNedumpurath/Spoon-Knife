8ebd23a4bedb1e5ae331a38f159e739b161cada9
AbhilashNedumpurath <toabhilashn@gmail.com>
2021-06-04 16:04:42 +0530

    Add files via upload

---
+++ Customer.cs
@@ -0,0 +1,36 @@
+﻿using SampleApp.Order;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SampleApp
+{
+    public class Customer
+    {
+        private List<IOrderItem> orders;
+
+        public Customer()
+        {
+            orders = new List<IOrderItem>();
+        }
+        public void Order(IOrderItem order)
+        {
+            orders.Add(order);
+        }
+        public void RemoveOrder(IOrderItem order)
+        {
+            orders.Remove(order);
+        }
+
+        public void DoPayment()
+        {
+            Console.WriteLine("Payment Done");
+
+            //Executing Rules
+            foreach(var order in orders)
+            {
+                order.ExecuteRules();
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using SampleApp.Order;
+using SampleApp.OrderItems;
+using SampleApp.Rules;
+using System;
+using System.Collections.Generic;
+
+namespace SampleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book.Rules = new List<IRule> {  new GenerateDuplicatePackingSlip(), new CommitionPayment() };
+            PhysicalItem.Rules = new List<IRule> { new GeneratePackingSlip(), new CommitionPayment() };
+            MembershipActivate.Rules = new List<IRule> { new ActivateMembership(), new EmailMembershipDetail() };
+            MembershipUpgrade.Rules = new List<IRule> { new UpgradeMembership(), new EmailMembershipDetail() };
+
+            Customer customer = new Customer();
+            customer.Order(new Book());
+            customer.Order(new MembershipActivate());
+            customer.DoPayment();
+
+            Console.ReadLine();
+        }
+    }
+}
